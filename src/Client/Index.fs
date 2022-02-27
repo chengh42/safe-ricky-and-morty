024980d75e55a7a7439f69c0ea60649aab5d875d
Hsin <hsin@impactinstitute.com>
2022-02-27 20:01:50 +0100

    update client

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -3,123 +3,141 @@ module Index
 open Elmish
 open Fable.Remoting.Client
 open Shared
+open SafeRickAndMorty
 
-type Model = { Todos: Todo list; Input: string }
+type AsyncOperationStatus<'t> =
+    | Started
+    | Finished of 't
+
+type Deferred<'t> =
+    | HasNotStartedYet
+    | InProgress
+    | Resolved of 't
+
+type Model = { Characters: Deferred<GetCharacters.Character list>; SearchValue: string }
 
 type Msg =
-    | GotTodos of Todo list
-    | SetInput of string
-    | AddTodo
-    | AddedTodo of Todo
+    | GetRickAndMortyCharacters of AsyncOperationStatus<Result<GetCharacters.Query,ErrorType list>>
+    | SetSearchValue of string
+    | SearchCharacter
 
-let todosApi =
-    Remoting.createApi ()
-    |> Remoting.withRouteBuilder Route.builder
-    |> Remoting.buildProxy<ITodosApi>
+let client = SafeRickAndMortyGraphqlClient("https://rickandmortyapi.com/graphql")
 
 let init () : Model * Cmd<Msg> =
-    let model = { Todos = []; Input = "" }
-
-    let cmd =
-        Cmd.OfAsync.perform todosApi.getTodos () GotTodos
-
+    let model = { Characters = HasNotStartedYet; SearchValue = "" }
+    let input: GetCharacters.InputVariables = { name = model.SearchValue }
+    let cmd = Cmd.OfAsync.perform client.GetCharacters input (Finished >> GetRickAndMortyCharacters)
     model, cmd
 
 let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
     match msg with
-    | GotTodos todos -> { model with Todos = todos }, Cmd.none
-    | SetInput value -> { model with Input = value }, Cmd.none
-    | AddTodo ->
-        let todo = Todo.create model.Input
-
+    | GetRickAndMortyCharacters Started -> model, Cmd.none
+    | GetRickAndMortyCharacters (Finished (Error e)) -> model, Cmd.none
+    | GetRickAndMortyCharacters (Finished (Ok queryResult)) ->
+        let characters =
+            queryResult.characters
+            |> Option.toList
+            |> List.collect (fun characters ->
+                characters.results
+                |> Option.toList
+                |> List.concat
+                |> List.choose id)
+        { model with Characters = Resolved characters }, Cmd.none
+    | SetSearchValue searchVal -> { model with SearchValue = searchVal }, Cmd.none
+    | SearchCharacter ->
         let cmd =
-            Cmd.OfAsync.perform todosApi.addTodo todo AddedTodo
+            Cmd.OfAsync.perform client.GetCharacters { name = model.SearchValue } (Finished >> GetRickAndMortyCharacters)
+        model, cmd
 
-        { model with Input = "" }, cmd
-    | AddedTodo todo ->
-        { model with
-              Todos = model.Todos @ [ todo ] },
-        Cmd.none
 
 open Feliz
 open Feliz.Bulma
 
-let navBrand =
-    Bulma.navbarBrand.div [
-        Bulma.navbarItem.a [
-            prop.href "https://safe-stack.github.io/"
-            navbarItem.isActive
-            prop.children [
-                Html.img [
-                    prop.src "/favicon.png"
-                    prop.alt "Logo"
+let displayOption<'t> (f: 't -> IReactProperty) (opt) = opt |> Option.toList |> (List.map f)
+let displayOptionString = displayOption<string> prop.text
+let displayOptionImage = displayOption prop.src
+
+let navBar =
+    Bulma.navbar [
+        prop.style [
+            style.backgroundColor "#FF3CAC"
+            style.backgroundImage "linear-gradient(225deg, #FF3CAC 0%, #784BA0 50%, #2B86C5 100%)"
+        ]
+        prop.children [
+            Bulma.navbarBrand.div [
+                Bulma.navbarItem.a [
+                    prop.style [ style.color "white"; style.fontSize 20 ]
+                    prop.text "Client side GraphQL"
                 ]
             ]
         ]
     ]
 
-let containerBox (model: Model) (dispatch: Msg -> unit) =
+let characterView (character: GetCharacters.Character) =
     Bulma.box [
-        Bulma.content [
-            Html.ol [
-                for todo in model.Todos do
-                    Html.li [ prop.text todo.Description ]
-            ]
+    prop.style [ style.height 300; style.width 200 ]
+    prop.children [
+        Html.img (displayOptionImage character.image)
+        Html.div (displayOptionString character.name)
+        Html.div (displayOptionString character.species)
+        Html.div (displayOptionString character.status)
         ]
-        Bulma.field.div [
-            field.isGrouped
-            prop.children [
-                Bulma.control.p [
-                    control.isExpanded
-                    prop.children [
-                        Bulma.input.text [
-                            prop.value model.Input
-                            prop.placeholder "What needs to be done?"
-                            prop.onChange (fun x -> SetInput x |> dispatch)
-                        ]
-                    ]
-                ]
-                Bulma.control.p [
-                    Bulma.button.a [
-                        color.isPrimary
-                        prop.disabled (Todo.isValid model.Input |> not)
-                        prop.onClick (fun _ -> dispatch AddTodo)
-                        prop.text "Add"
-                    ]
-                ]
-            ]
+    ]
+
+let charactersView (characters: GetCharacters.Character list) (dispatch: Msg -> unit) =
+    Bulma.columns [
+        columns.isMultiline
+        prop.children [
+        for character in characters do
+        Bulma.column [
+            characterView character
+        ]
         ]
     ]
 
 let view (model: Model) (dispatch: Msg -> unit) =
-    Bulma.hero [
-        hero.isFullHeight
-        color.isPrimary
-        prop.style [
-            style.backgroundSize "cover"
-            style.backgroundImageUrl "https://unsplash.it/1200/900?random"
-            style.backgroundPosition "no-repeat center center fixed"
-        ]
+    Html.div [
         prop.children [
-            Bulma.heroHead [
-                Bulma.navbar [
-                    Bulma.container [ navBrand ]
-                ]
-            ]
-            Bulma.heroBody [
-                Bulma.container [
-                    Bulma.column [
-                        column.is6
-                        column.isOffset3
-                        prop.children [
-                            Bulma.title [
-                                text.hasTextCentered
-                                prop.text "safe_ricky_and_morty"
+            navBar
+            Bulma.container [
+                prop.style [
+                    style.marginTop 50
+                    style.display.flex
+                    style.justifyContent.center
+                    style.alignContent.center
+                    style.flexDirection.column ]
+                prop.children [
+                    Bulma.box [
+                        Bulma.columns [
+                            Bulma.column [
+                                column.is10
+                                prop.children [
+                                    Bulma.input.search [
+                                        prop.value model.SearchValue
+                                        prop.onChange (SetSearchValue >> dispatch)
+                                    ]
+                                ]
                             ]
-                            containerBox model dispatch
+                            Bulma.column [
+                                column.is2
+                                prop.children [
+                                    Bulma.button.button [
+                                        prop.style [ style.width (length.percent 100) ]
+                                        prop.text "Search"
+                                        prop.onClick (fun _ -> SearchCharacter |> dispatch)
+                                    ]
+                                ]
+                            ]
                         ]
                     ]
-                ]
+                    match model.Characters with
+                    | HasNotStartedYet -> Html.div "Has not started"
+                    | InProgress -> Html.div "Loading"
+                    | Resolved characters ->
+                        match characters with
+                        | [] -> Html.div "No characters with that name"
+                        | _ -> charactersView characters dispatch
+                    ]
             ]
         ]
     ]
